62582399bf6c16deae99e31615c6afa2dde6f120
deip-29 <deepikacs297@gmail.com>
2026-04-09 23:46:04 +0530

    app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./components/Header";
+
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import NotFound from "./pages/NotFound";
 
 function Layout({ children }) {
   return (
@@ -9,7 +17,14 @@ function Layout({ children }) {
   );
 }
 
-
+const router = createBrowserRouter([
+  { path: "/", element: <Layout><Home /></Layout> },
+  { path: "/products", element: <Layout><ProductList /></Layout> },
+  { path: "/product/:id", element: <Layout><ProductDetail /></Layout> },
+  { path: "/cart", element: <Layout><Cart /></Layout> },
+  { path: "/checkout", element: <Layout><Checkout /></Layout> },
+  { path: "*", element: <NotFound /> }
+]);
 
 export default function App() {
   return <RouterProvider router={router} />;
